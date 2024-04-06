8f17f08ae12f728d56327cdebff2065bf074dc52
dd-jiyun <dlwldbs8015@gmail.com>
2024-04-06 23:49:45 +0900

    feat: 자동차 생성 구현
    - 자동차 이름은 5자를 초과할 수 없습니다.

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,4 +1,33 @@
 package racingcar.model;
 
 public class Car {
+
+    private static final int MAX_NAME_LENGTH = 5;
+
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("자동차 이름은 5자를 초과할 수 없습니다.");
+        }
+    }
+
+    public void moveForward() {
+        position++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
 }
