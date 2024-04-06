ee031a3d1d0b16113d947f5a21dbf7d92231d618
Yehyeok Bang <qkddpgur318@gmail.com>
2024-04-07 00:31:16 +0900

    feat: 자동차들 구현
    - 규칙에 따라 자동차들을 전진시키는 기능을 추가합니다.
    - 무작위 숫자 생성기를 추가합니다.

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,4 +1,38 @@
 package racingcar.model;
 
+import racingcar.util.RandomNumberGenerator;
+
+import java.util.List;
+
 public class Cars {
+
+    private static final int MIN_BOUND = 0;
+    private static final int MAX_BOUND = 9;
+
+    private final List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public void moveEligibleCars(MoveRule moveRule) {
+        for (Car car : cars) {
+            if (moveRule.canMove(RandomNumberGenerator.generateRandomNumber(MIN_BOUND, MAX_BOUND))) {
+                car.moveForward();
+            }
+        }
+    }
+
+    public List<Integer> getPositions() {
+        return cars.stream()
+                .map(Car::getPosition)
+                .toList();
+    }
+
+    public List<String> getNames() {
+        return cars.stream()
+                .map(Car::getName)
+                .toList();
+    }
+
 }
---
+++ src/main/java/racingcar/util/RandomNumberGenerator.java
@@ -0,0 +1,15 @@
+package racingcar.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberGenerator {
+
+    private RandomNumberGenerator() {
+
+    }
+
+    public static int generateRandomNumber(int minBound, int maxBound) {
+        return Randoms.pickNumberInRange(minBound, maxBound);
+    }
+
+}
