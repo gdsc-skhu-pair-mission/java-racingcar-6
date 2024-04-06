53783242ba119b6d9037a5685b8d0ba875be885b
Yehyeok Bang <qkddpgur318@gmail.com>
2024-04-07 00:19:48 +0900

    test: 자동차들 관련 테스트 추가

---
+++ src/test/java/racingcar/model/CarsTest.java
@@ -0,0 +1,48 @@
+package racingcar.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarsTest {
+
+    private Car testCar1;
+    private Car testCar2;
+
+    @BeforeEach
+    void setUp() {
+        testCar1 = new Car("pobi");
+        testCar2 = new Car("woni");
+    }
+
+    @Test
+    void 정상적으로_자동차들_객체_생성() {
+        // given
+        List<Car> testCars = List.of(testCar1, testCar2);
+
+        // when
+        Cars cars = new Cars(testCars);
+
+        // then
+        assertThat(cars).isNotNull();
+    }
+
+    @Test
+    void 자동차들이_규칙에_따라_전진() {
+        // given
+        List<Car> testCars = List.of(testCar1, testCar2);
+        Cars cars = new Cars(testCars);
+
+        // when
+        cars.moveEligibleCars(MoveRule.ALWAYS_MOVE);
+
+        // then
+        for (int position : cars.getPositions()) {
+            assertThat(position).isEqualTo(1);
+        }
+    }
+
+}
