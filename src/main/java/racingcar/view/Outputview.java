1d128f32c99c01585a486722a55df0a2ca047630
Junyeong-An <2117ab@gmail.com>
2024-04-06 23:49:13 +0900

    fix: 우승자 출력 메소드 추가

--- src/main/java/racingcar/view/Outputview.java
+++ src/main/java/racingcar/view/Outputview.java
@@ -1,5 +1,9 @@
 package racingcar.view;
 
+import racingcar.model.Car;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Outputview {
     public void StartMessage(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -15,5 +19,15 @@ public class Outputview {
 
     public String GoMessage(){
         return "-";
+    }
+
+    // 우승자 출력
+    public void PrintWinner(List<Car> winners){
+       // String 객체에 Car객체를 , 단위로 나누어서 담음
+        String stringWinners = winners.stream()
+                .map(car -> car.getName())
+                .collect(Collectors.joining(", "));
+        System.out.println("최종 우승자 : " + stringWinners );
+
     }
 }
