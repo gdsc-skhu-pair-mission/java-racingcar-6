74a0aff0d4f9bf25d2280dafcc619823fafc82ee
dongkyunKim <kyun010713@gmail.com>
2024-04-08 23:51:32 +0900

    feat(view) : InputView 추가

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,35 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.exception.ValidateInputData;
+
+public class InputView {
+
+    private static InputView instance;
+    private final String CAR_NAME_INPUT_PROMPT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final String ATTEMPT_PROMPT_MESSAGE = "시도할 회수는 몇 회인가요?";
+
+    public static InputView getInstance() {
+        if (instance == null) {
+            instance = new InputView();
+        }
+        return instance;
+    }
+
+    public String[] inputRacingCarName() {
+        System.out.println(CAR_NAME_INPUT_PROMPT_MESSAGE);
+        String[] carNames = Console.readLine().split(",");
+        ValidateInputData.validateCarNameIsCorrect(carNames);
+
+        return carNames;
+    }
+
+    public int inputAttemptCount() {
+        System.out.println(ATTEMPT_PROMPT_MESSAGE);
+        String tryNumber = Console.readLine();
+
+        ValidateInputData.validateTryNumberIsCorrect(tryNumber);
+
+        return Integer.parseInt(tryNumber);
+    }
+}
